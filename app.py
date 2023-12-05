0b2e3c8230660dd030efe5a6c0e5c94c15e21760
Wufei Ma <wufeim@gmail.com>
2023-12-04 22:34:03 -0500

    update docs

--- app.py
+++ app.py
@@ -1151,5 +1151,5 @@ def text():
 
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=6260, debug=False)
-    app.run()
+    app.run(host='0.0.0.0', port=6260, debug=False)
+    # app.run()
